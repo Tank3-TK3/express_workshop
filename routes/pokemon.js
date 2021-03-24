918de86ef19a37cb766b04a7c05515c566b35f8c
xTank3x <robertocruzlozano97@hotmail.com>
2021-03-23 18:49:38 -0600

    pokemon update 23/03/2021

---
+++ routes/pokemon.js
@@ -0,0 +1,32 @@
+const expres = require('express');
+const pokemon = expres.Router();
+const db = require('../config/database');
+
+pokemon.post("/", (req, res, next) => {
+    return res.status(200).send(req.body);
+});
+
+pokemon.get('/', async (req, res, next) => {
+    const pkmn = await db.query("SELECT * FROM pokemon");
+    return res.status(200).json(pkmn);
+});
+
+pokemon.get('/:id([0-9]{1,3})', async (req, res, next) => {
+    const id = req.params.id;
+    const pkmn = await db.query("SELECT * FROM pokemon WHERE pok_id = " + id);
+    console.log(pkmn);
+    (id >= 1 && id <= 722) ? 
+        res.status(200).json(pkmn) : 
+        res.status(404).send('Pokemon no encontrado');
+});
+
+pokemon.get('/:name([A-Za-z]+)', async (req, res, next) => {
+    const name = req.params.name;
+    const pkmn = await db.query("SELECT * FROM pokemon WHERE pok_name = '"+name.toLowerCase()+"'");
+    console.log(pkmn);
+    (pkmn.length > 0) ? 
+        res.status(200).json(pkmn) : 
+        res.status(404).send('Pokemon no encontrado');
+});
+
+module.exports = pokemon;
