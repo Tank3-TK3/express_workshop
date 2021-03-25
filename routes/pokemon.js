64fba8852f04eaa31aa2aa2e22753249c090040f
xTank3x <robertocruzlozano97@hotmail.com>
2021-03-25 16:47:49 -0600

    pokemon update 25/03/2021

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -3,30 +3,28 @@ const pokemon = expres.Router();
 const db = require('../config/database');
 
 pokemon.post("/", (req, res, next) => {
-    return res.status(200).send(req.body);
+    return res.status(200).json(req.body);
 });
 
 pokemon.get('/', async (req, res, next) => {
     const pkmn = await db.query("SELECT * FROM pokemon");
-    return res.status(200).json(pkmn);
+    return res.status(200).json({ code: 1, message: pkmn });
 });
 
 pokemon.get('/:id([0-9]{1,3})', async (req, res, next) => {
     const id = req.params.id;
-    const pkmn = await db.query("SELECT * FROM pokemon WHERE pok_id = " + id);
-    console.log(pkmn);
+    const pkmn = await db.query("SELECT * FROM pokemon WHERE pok_id = " + id + ";");
     (id >= 1 && id <= 722) ? 
-        res.status(200).json(pkmn) : 
-        res.status(404).send('Pokemon no encontrado');
+        res.status(200).json({ code: 1, message: pkmn }) : 
+        res.status(404).json({ code: 404, message: "Pokemon no encontrado" });
 });
 
 pokemon.get('/:name([A-Za-z]+)', async (req, res, next) => {
     const name = req.params.name;
-    const pkmn = await db.query("SELECT * FROM pokemon WHERE pok_name = '"+name.toLowerCase()+"'");
-    console.log(pkmn);
+    const pkmn = await db.query("SELECT * FROM pokemon WHERE pok_name = '"+name.toLowerCase()+"';");
     (pkmn.length > 0) ? 
-        res.status(200).json(pkmn) : 
-        res.status(404).send('Pokemon no encontrado');
+        res.status(200).json({ code: 1, message: pkmn }) : 
+        res.status(404).send({ code: 404, message: "Pokemon no encontrado" });
 });
 
 module.exports = pokemon;
